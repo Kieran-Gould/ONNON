25d568b00ece5751f03f1d38b99a4362638637e0
Kieran Gould <kierang@largepixelcollider.net>
2017-03-14 12:52:09 +0000

    More changes
    Added enemy AI
    Working on enemy damage
    Bullets despawn with time
    Environment improvement on the front wall

--- Assets/scripts/test/BulletController.cs
+++ Assets/scripts/test/BulletController.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour {
 
-	public float speed;															// Speed of the projectile													
+	public float speed;															// Speed of the projectile			
+	private float lifetime = 5;													// Bullet lifetime
+
+	void Start () {
+		Destroy(gameObject, lifetime);
+	}
 
 	void Update () {
 		transform.Translate(Vector3.forward * speed * Time.deltaTime); 			// Translates the bullet forward, based on it's speed, using delta time to ensure it doesn't jitter
---
+++ Assets/scripts/test/enemyController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyController : MonoBehaviour {
+
+	private Rigidbody myRB;
+	public float movementSpeed;
+	public PlayerController thePlayer;
+	public int health;
+	private int currentHealth;
+
+	void Start () {
+		myRB = GetComponent<Rigidbody>();					// Get player collision
+		thePlayer = FindObjectOfType<PlayerController>();	// Get player controller
+	}
+
+	void FixedUpdate () {
+		myRB.velocity = (transform.forward * movementSpeed);
+	}
+
+	public void hurtEnemy(int damage) {
+		currentHealth -= damage;
+	}
+
+	void Update () {
+		if (currentHealth <= 0) {
+			Destroy (gameObject);
+		}
+
+		transform.LookAt (thePlayer.transform.position);	// Turn to look at player
+	}
+}
